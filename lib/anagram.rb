68ed7cd0e8030fbebe9dd6dfdda1a3b32622ba86
Victoria B <viktorija.bourassa@gmail.com>
2019-01-23 15:39:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,12 +6,12 @@ class Anagram
   end
 
   def match(array)
+    newArr = []
     array.each do |anagram|
       if @word.chars.sort(&:casecmp).join == anagram.chars.sort(&:casecmp).join
-        return true
-      else
-        false
+        newArr << anagram
       end
+      return newArr
     end
   end
 
